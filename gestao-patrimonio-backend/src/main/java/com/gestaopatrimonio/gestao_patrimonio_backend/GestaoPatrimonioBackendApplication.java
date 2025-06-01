04cff4a41096e2ca97ac559955b1f963e2ee1183
GitArthurMarchetti <avinhasmarhchetti@gmail.com>
2025-05-31 21:40:09 -0300

    First Commit

---
+++ gestao-patrimonio-backend/src/main/java/com/gestaopatrimonio/gestao_patrimonio_backend/GestaoPatrimonioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gestaopatrimonio.gestao_patrimonio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoPatrimonioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoPatrimonioBackendApplication.class, args);
+	}
+
+}
---
+++ gestao-patrimonio-backend/src/test/java/com/gestaopatrimonio/gestao_patrimonio_backend/GestaoPatrimonioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestaopatrimonio.gestao_patrimonio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoPatrimonioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
